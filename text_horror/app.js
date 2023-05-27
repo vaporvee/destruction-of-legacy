bfadafa86f2a4632c04251f04bd1b9d364e136ac
Yannik <80621863+vaporvee@users.noreply.github.com>
2023-05-27 11:35:42 +0200

    improved clikc area triangle hiding and text wrapping

--- text_horror/app.js
+++ text_horror/app.js
@@ -15,31 +15,33 @@ function load() {
 }
 
 function typeWriter() {
-  if (document.getElementById("bubble").innerHTML.length < dlgLines[dlgPointer].toString().length) {
-    document.getElementById("bubble").innerHTML += dlgLines[dlgPointer].toString().charAt(typeindex);
+  if (document.getElementById("dlg-text").innerHTML.length < dlgLines[dlgPointer].toString().length) {
+    document.getElementById("dlg-text").innerHTML += dlgLines[dlgPointer].toString().charAt(typeindex);
     typeindex++;
-    document.getElementById("triangle").hidden = document.getElementById("bubble").innerHTML.length != dlgLines[dlgPointer].length
+    document.getElementById("triangle").hidden = document.getElementById("dlg-text").innerHTML.length != dlgLines[dlgPointer].length
     setTimeout(typeWriter, speed); //loops because of running "typeWriter" after waiting
   }
 }
+
+document.addEventListener('mousedown', mouseDown, false)
+document.addEventListener('mouseup', mouseUp, false)
 
 function mouseDown() {
   document.getElementById("triangle").hidden = true;
 }
 
 function mouseUp() {
-  if (document.getElementById("bubble").innerHTML.length == dlgLines[dlgPointer].length) { //check if text is typed out
+  if (document.getElementById("dlg-text").innerHTML.length == dlgLines[dlgPointer].length) { //check if text is typed out
+    document.getElementById("triangle").hidden = true;
     if (dlgPointer < dlgLines.length - 1) { //check if dlgPointer is not at the array end
       do
         dlgPointer++;
       while (!(typeof dlgLines[dlgPointer] === 'string')) //again if it's not string
       typeindex = 0;
-      document.getElementById("bubble").innerHTML = "";
-      document.getElementById("triangle").hidden = true;
+      document.getElementById("dlg-text").innerHTML = "";
       typeWriter();
     } else {
       document.getElementById("bubble").hidden = true;
-      document.getElementById("triangle").hidden = true;
     }
   }
 }
